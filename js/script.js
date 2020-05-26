89721c92e17e4f51ee11f3f1907d18089c44ef2e
Klimov-V <klimov_vitaliy@icloud.com>
2020-05-26 23:07:49 +0300

    finished first ex

--- js/script.js
+++ js/script.js
@@ -1,47 +1,56 @@
 (function() {
     'use strict'
 
-    let root = document.querySelector('#root'),
-        ul = document.createElement('ul'),
-        li = document.createElement('li'),
-        playList = [
-        {
-         author: "LED ZEPPELIN",
-         song:"STAIRWAY TO HEAVEN"
-        }, {
-         author: "QUEEN",
-         song:"BOHEMIAN RHAPSODY"
-        }, {
-         author: "LYNYRD SKYNYRD",
-         song:"FREE BIRD"
-        }, {
-         author: "DEEP PURPLE",
-         song:"SMOKE ON THE WATER"
-        }, {
-         author: "JIMI HENDRIX",
-         song:"ALL ALONG THE WATCHTOWER"
-        }, {
-         author: "AC/DC",
-         song:"BACK IN BLACK"
-        }, {
-         author: "QUEEN",
-         song:"WE WILL ROCK YOU"
-        }, {
-         author: "METALLICA",
-         song:"ENTER SANDMAN"
-        }];
-    
-    root.append(ul);
+    // 1
 
-    console.log(playList.length);
+    // let root = document.querySelector('#root'),
+    //     ol = document.createElement('ol'),
+    //     playList = [
+    //     {
+    //      author: "LED ZEPPELIN",
+    //      song:"STAIRWAY TO HEAVEN"
+    //     }, {
+    //      author: "QUEEN",
+    //      song:"BOHEMIAN RHAPSODY"
+    //     }, {
+    //      author: "LYNYRD SKYNYRD",
+    //      song:"FREE BIRD"
+    //     }, {
+    //      author: "DEEP PURPLE",
+    //      song:"SMOKE ON THE WATER"
+    //     }, {
+    //      author: "JIMI HENDRIX",
+    //      song:"ALL ALONG THE WATCHTOWER"
+    //     }, {
+    //      author: "AC/DC",
+    //      song:"BACK IN BLACK"
+    //     }, {
+    //      author: "QUEEN",
+    //      song:"WE WILL ROCK YOU"
+    //     }, {
+    //      author: "METALLICA",
+    //      song:"ENTER SANDMAN"
+    //     }];
     
-    for (let i = 0; i < 8; i++) {
-        // li.innerHTML = playList[i].author;
-        document.querySelector('ul').append(li);
-        console.log('печать');
+    // root.append(ol);
+    
+    // for (let i = 0; i < 8; i++) {
+    //     let li = document.createElement('li');
+    //     ol.append(li);
+    //     li.innerHTML = `<strong>${playList[i].author}</strong> - ${playList[i].song}`;
+    // }
+
+
+    // 2
+
+    let btn = document.querySelector('#btn'),
+        popup = document.querySelector('#popup');
+
+    function openPopup() {
+        popup.setAttribute('hidden', 'false');
     }
+
+    btn.addEventListener(onclick, openPopup);
 
     
 })();
-
-//.innerHTML = playList[i].author
